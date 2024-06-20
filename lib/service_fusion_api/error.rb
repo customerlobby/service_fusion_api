0c51fa27bdc9e2fcbe43d4a385f98d4d3626a1b2
Aakash Kumar Mishra <amishra@beesolvertechnology.com>
2024-06-20 10:54:27 +0530

    CA-15760 : ServiceFusionApi  : ServiceFusionApi Authentication issue

--- lib/service_fusion_api/error.rb
+++ lib/service_fusion_api/error.rb
@@ -3,7 +3,6 @@
 module ServiceFusionApi
   class Error < StandardError; end
   class ConnectionError < Error; end
-  class AuthorizationError < Error; end
   class BadRequestError < Error; end
   class RecordNotFoundError < Error; end
   class GatewayTimeoutError < Error; end
@@ -17,6 +16,15 @@ module ServiceFusionApi
       @status_code = http_response.body.code
       @rate_limit_reset_at = Time.current.utc +
                              http_response.headers['X-Rate-Limit-Reset'].to_i
+    end
+  end
+
+  class AuthorizationError < Error
+    attr_reader :status_code
+    def initialize(http_response)
+      super(http_response.body.message)
+
+      @status_code = http_response.body.code
     end
   end
 end
